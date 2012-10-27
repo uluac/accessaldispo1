39e3a4eca10c822ee58fecfa387ce13a8d8a4dc5
erick uluac mijangos <uluac@hotmail.com>
2012-10-27 14:25:23 -0500

    cambio de etiqueta
    se cambio device por devic en las etiquetas

--- appjs/actions.js
+++ appjs/actions.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 function deviceinfo(){
-	$('#device table td').eq(1).text(device.name);
-	$('#device table td').eq(3).text(device.phonegap);
-	$('#device table td').eq(5).text(device.plataform);
-	$('#device table td').eq(7).text(device.version);
-	$('#device table td').eq(9).text(device.uuid);
+	$('#devic table td').eq(1).text(device.name);
+	$('#devic table td').eq(3).text(device.phonegap);
+	$('#devic table td').eq(5).text(device.plataform);
+	$('#devic table td').eq(7).text(device.version);
+	$('#devic table td').eq(9).text(device.uuid);
 }
 $(document).ready(function(){
 	deviceinfo();
